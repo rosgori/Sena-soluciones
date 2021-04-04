8cf8f16febc61f30f2a3e579693d0ebd941167d4
David Cortés <33338133+rosgori@users.noreply.github.com>
2021-04-03 23:55:31 -0500

    El archivo que conecta la base de datos

---
+++ Trim-II/SolNotasBD/Modelo/Taller_estudiantesContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SolNotasBD.Modelo
+{
+    public partial class Taller_estudiantesContext : DbContext
+    {
+        public Taller_estudiantesContext()
+        {
+        }
+
+        public Taller_estudiantesContext(DbContextOptions<Taller_estudiantesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudiante> Estudiantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;database=;user=;password=", Microsoft.EntityFrameworkCore.ServerVersion.FromString("8.0.23-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudiante>(entity =>
+            {
+                entity.HasKey(e => e.Codigo)
+                    .HasName("PRIMARY");
+
+                entity.Property(e => e.Codigo)
+                    .ValueGeneratedNever()
+                    .HasColumnName("codigo");
+
+                entity.Property(e => e.Correo)
+                    .HasColumnType("varchar(30)")
+                    .HasColumnName("correo")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Nombre)
+                    .HasColumnType("varchar(45)")
+                    .HasColumnName("nombre")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Nota1).HasColumnName("nota1");
+
+                entity.Property(e => e.Nota2).HasColumnName("nota2");
+
+                entity.Property(e => e.Nota3).HasColumnName("nota3");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
